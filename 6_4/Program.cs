167119e94d36950f5a9729825182af66f4baef40
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2017-09-22 19:51:29 +0300

    solved task for displaying only unique numbers

--- 6_4/Program.cs
+++ 6_4/Program.cs
@@ -15,7 +15,35 @@ namespace _6_4
              * numbers may include duplicates. Display the unique
              * numbers that the user has entered. */
 
+            var numbers = new List<int>();
+            string input;
+            int temp_number;
+            bool good;
 
+            Console.WriteLine("Continuously enter numbers or type 'Quit' to exit.");
+
+            do
+            {
+                input = Console.ReadLine();
+                good = int.TryParse(input, out temp_number);
+                if (good)
+                {
+                    numbers.Add(temp_number);
+                }
+            } while (good);
+
+            var unique_numbers = new List<int>();
+
+            foreach (var number in numbers)
+            {
+                if (!unique_numbers.Contains(number))
+                {
+                    unique_numbers.Add(number);
+                }
+            }
+
+            var output = string.Join(", ", unique_numbers);
+            Console.WriteLine("All unique numbers: " + output);
         }
     }
 }
