024d91ad3065ccd9044a61c42ad76a7657224960
Ieva <ieva.jaksaityte@gmail.com>
2017-09-22 18:45:36 +0300

    added last task project of section 6

---
+++ 6_5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 5 - Write a program and ask the user to supply a list
+             * of comma separated numbers (e.g 5, 1, 9, 2, 10). If the
+             * list is empty or includes less than 5 numbers, display
+             * "Invalid List" and ask the user to re-try; otherwise,
+             * display the 3 smallest numbers in the list.  */
+
+
+        }
+    }
+}
