5aeaba7ae0a39231d868ed450a5d9abfc10ac276
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2017-09-22 22:08:00 +0300

    find longest word in a text file

---
+++ 9_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 2 - Write a program that reads a text
+             * file and displays the longest word in
+             * the file. */
+
+            var text = File.ReadAllLines("Text.txt");
+            var longest = "";
+
+            foreach (var line in text)
+            {
+                var words = line.Split(' ', ',', '.');
+                foreach (var word in words)
+                {
+                    if (word.Length > longest.Length)
+                    {
+                        longest = word;
+                    }
+                }
+            }
+
+            Console.WriteLine("Longest word: " + longest);
+        }
+    }
+}
